f16b1064e811ffa39e5e20549de585bcca5153cf
L7-BUG <ll789y@gmail.com>
2025-11-14 19:05:47 +0800

    root init

---
+++ system-infrastructure/src/test/java/com/l7bug/system/mybatis/service/SystemUserServiceTest.java
@@ -0,0 +1,26 @@
+package com.l7bug.system.mybatis.service;
+
+import com.l7bug.system.domain.user.User;
+import com.l7bug.system.domain.user.UserGateway;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemUserServiceTest {
+	@Autowired
+	private UserGateway userGateway;
+
+	@Test
+	void saveRoot() {
+		User root = userGateway.getUserByUsername("root");
+		if (root == null) {
+			root = new User(userGateway);
+			root.setUsername("root");
+			root.setNickname("root");
+			root.setRawPassword("123456");
+			root.setEnable();
+		}
+		root.save();
+	}
+}
