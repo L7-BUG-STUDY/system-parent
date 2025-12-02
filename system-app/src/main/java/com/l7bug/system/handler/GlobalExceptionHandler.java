8b3e2fc295e327f74aa2038972d8be2b2bdbe704
L7-BUG <ll789y@gmail.com>
2025-12-02 11:18:33 +0800

    调整全局异常捕获

--- system-app/src/main/java/com/l7bug/system/handler/GlobalExceptionHandler.java
+++ system-app/src/main/java/com/l7bug/system/handler/GlobalExceptionHandler.java
@@ -3,8 +3,6 @@ package com.l7bug.system.handler;
 
 import com.google.common.base.Strings;
 import com.l7bug.common.error.ClientErrorCode;
-import com.l7bug.common.error.ServerErrorCode;
-import com.l7bug.common.exception.AbstractException;
 import com.l7bug.common.result.Result;
 import com.l7bug.common.result.Results;
 import jakarta.servlet.http.HttpServletRequest;
@@ -35,34 +33,6 @@ public class GlobalExceptionHandler {
 	public Result<Void> internalAuthenticationServiceException(DisabledException ex) {
 		log.warn("捕获用户被禁用异常", ex);
 		return Results.failure(ClientErrorCode.USER_IS_DISABLE);
-	}
-
-	/**
-	 * 拦截应用内抛出的异常
-	 */
-	@ExceptionHandler(value = {AbstractException.class})
-	public Result<Void> abstractException(HttpServletRequest request, AbstractException ex) {
-		if (ex.getCause() != null) {
-			log.error("[{}] {} [ex] {}", request.getMethod(), request.getRequestURL().toString(), ex, ex.getCause());
-			return Results.failure(ex.getCode(), ex.getMessage());
-		}
-		StringBuilder stackTraceBuilder = new StringBuilder();
-		stackTraceBuilder.append(ex.getClass().getName()).append(": ").append(ex.getCode()).append("\n");
-		StackTraceElement[] stackTrace = ex.getStackTrace();
-		for (int i = 0; i < Math.min(5, stackTrace.length); i++) {
-			stackTraceBuilder.append("\tat ").append(stackTrace[i]).append("\n");
-		}
-		log.error("[{}] {} [ex] {} \n\n{}", request.getMethod(), request.getRequestURL().toString(), ex, stackTraceBuilder);
-		return Results.failure(ex.getCode(), ex.getMessage());
-	}
-
-	/**
-	 * 拦截未捕获异常
-	 */
-	@ExceptionHandler(value = Throwable.class)
-	public Result<String> defaultErrorHandler(HttpServletRequest request, Throwable throwable) {
-		log.error("[{}] {} ", request.getMethod(), getUrl(request), throwable);
-		return Results.failure(ServerErrorCode.SERVER_ERROR.getCode(), throwable.getMessage());
 	}
 
 	private String getUrl(HttpServletRequest request) {
