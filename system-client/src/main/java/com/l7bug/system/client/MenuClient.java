2734aa73dc9636fb41e8bc7cf5b7375312b829ee
L7-BUG <ll789y@gmail.com>
2025-12-03 18:29:17 +0800

    菜单resp

--- system-client/src/main/java/com/l7bug/system/client/MenuClient.java
+++ system-client/src/main/java/com/l7bug/system/client/MenuClient.java
@@ -1,5 +1,9 @@
 package com.l7bug.system.client;
 
+import com.l7bug.system.dto.response.MenuNodeResponse;
+
+import java.util.List;
+
 /**
  * MenuClient
  *
@@ -7,5 +11,10 @@ package com.l7bug.system.client;
  * @since 2025/12/3 18:00
  */
 public interface MenuClient {
-
+	/**
+	 * 获取全部根节点以及子节点数据
+	 *
+	 * @return 根节点数据
+	 */
+	List<MenuNodeResponse> getAllRootNodes();
 }
--- system-client/src/main/java/com/l7bug/system/dto/response/MenuNodeResponse.java
+++ system-client/src/main/java/com/l7bug/system/dto/response/MenuNodeResponse.java
@@ -2,6 +2,8 @@ package com.l7bug.system.dto.response;
 
 import lombok.Data;
 
+import java.util.List;
+
 /**
  * MenuNode
  *
@@ -10,8 +12,47 @@ import lombok.Data;
  */
 @Data
 public class MenuNodeResponse {
+	/**
+	 * 父id
+	 */
+	private Long fatherId;
+
+	/**
+	 * 全路径id
+	 */
+	private String fullId;
+
+	/**
+	 * 路径
+	 */
 	private String path;
+
+	/**
+	 * 名称
+	 */
 	private String name;
+
+	/**
+	 * 组件路径
+	 */
 	private String component;
+
+	/**
+	 * 菜单类型
+	 */
+	private String type;
+
+	/**
+	 * 启用状态
+	 */
+	private Boolean enable;
+
+	/**
+	 * 排序值
+	 */
+	private Integer sort;
+
 	private Meta meta;
+
+	private List<MenuNodeResponse> children;
 }
