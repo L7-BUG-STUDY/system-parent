8d40912714230212e2318723996c4c53835bd11f
L7-BUG <ll789y@gmail.com>
2025-12-04 18:17:34 +0800

    定义参数校验

--- system-client/src/main/java/com/l7bug/system/dto/request/MenuNodeRequest.java
+++ system-client/src/main/java/com/l7bug/system/dto/request/MenuNodeRequest.java
@@ -2,6 +2,8 @@ package com.l7bug.system.dto.request;
 
 import com.l7bug.system.dto.base.MenuType;
 import com.l7bug.system.dto.response.Meta;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -21,11 +23,13 @@ public class MenuNodeRequest {
 	/**
 	 * 路径
 	 */
+	@NotBlank(message = "路径不能为空")
 	private String path;
 
 	/**
 	 * 名称
 	 */
+	@NotBlank(message = "名称不能为空")
 	private String name;
 
 	/**
@@ -36,20 +40,23 @@ public class MenuNodeRequest {
 	/**
 	 * 菜单类型
 	 */
+	@NotNull(message = "菜单类型不能为空")
 	private MenuType type;
 
 	/**
 	 * 启用状态
 	 */
+	@NotNull(message = "启用状态不能为空")
 	private Boolean enable;
 
 	/**
 	 * 排序值
 	 */
-	private Integer sort;
+	private Integer sort = 0;
 
 	/**
 	 * 元数据
 	 */
+	@NotNull(message = "元数据不能为空")
 	private Meta meta;
 }
