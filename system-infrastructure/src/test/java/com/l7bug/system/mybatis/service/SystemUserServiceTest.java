d690b35f48036fffdda22f68433165c3ca6e4363
L7-BUG <ll789y@gmail.com>
2025-12-05 15:31:43 +0800

    开启虚拟线程

--- system-infrastructure/src/test/java/com/l7bug/system/mybatis/service/SystemUserServiceTest.java
+++ system-infrastructure/src/test/java/com/l7bug/system/mybatis/service/SystemUserServiceTest.java
@@ -1,8 +1,11 @@
 package com.l7bug.system.mybatis.service;
 
+import cn.hutool.core.util.IdUtil;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.l7bug.system.domain.user.User;
 import com.l7bug.system.domain.user.UserGateway;
+import com.l7bug.system.mybatis.dataobject.SystemUser;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -26,10 +29,20 @@ class SystemUserServiceTest {
 		root.setNickname("root");
 		root.setRawPassword("123456");
 		root.save();
+		User admin = userGateway.getUserByUsername("admin");
+
+		if (admin == null) {
+			admin = new User(userGateway);
+			admin.setUsername("admin");
+		}
+		admin.setEnable();
+		admin.setNickname(IdUtil.getSnowflakeNextIdStr());
+		admin.setRawPassword(admin.getNickname());
+		admin.save();
 	}
 
 	@Test
 	void all() throws JsonProcessingException {
-		systemUserService.list().forEach(System.err::println);
+		systemUserService.list(Wrappers.lambdaQuery(SystemUser.class).last("limit 10")).forEach(System.err::println);
 	}
 }
