bc5a6dc6bad8b03547a343ffb2d8fc453c470a7f
L7-BUG <ll789y@gmail.com>
2025-12-17 13:54:27 +0800

    test(menu): 验证菜单父子关系约束规则
    - 添加测试用例验证PAGE类型菜单只能作为根节点或FOLDER子节点
    - 添加测试用例验证BUTTON类型菜单只能作为PAGE子节点
    - 添加测试用例验证FOLDER类型菜单只能作为根节点或FOLDER子节点
    - 测试非法父子关系组合，确保抛出相应异常
    - 验证按钮不能添加到不存在的节点或文件夹下
    - 验证页面不能作为其他页面的子节点
    - 验证文件夹不能作为页面的子节点

--- system-domain/src/test/java/com/l7bug/system/domain/menu/MenuFatherTypeTest.java
+++ system-domain/src/test/java/com/l7bug/system/domain/menu/MenuFatherTypeTest.java
@@ -25,44 +25,64 @@ public class MenuFatherTypeTest extends BaseMenuTest {
 		root.save();
 	}
 
+	/**
+	 * 测试菜单保存功能及其父子关系约束
+	 * 验证不同类型菜单之间的父子关系约束规则：
+	 * 1. 页面(PAGE)只能作为根节点或文件夹(FOLDER)的子节点
+	 * 2. 按钮(BUTTON)只能作为页面(PAGE)的子节点
+	 * 3. 文件夹(FOLDER)只能作为根节点或文件夹(FOLDER)的子节点
+	 */
 	@Test
 	void saveTest() {
+		// 创建一个页面菜单并保存，父节点为根节点
 		Menu page = new Menu(menuGateway);
 		page.setName(UUID.randomUUID().toString());
 		page.setType(MenuType.PAGE);
 		page.setFatherId(root.getId());
 		page.save();
 
+		// 创建一个按钮菜单并保存，父节点为页面
 		Menu button = new Menu(menuGateway);
 		button.setName(UUID.randomUUID().toString());
 		button.setType(MenuType.BUTTON);
 		button.setFatherId(page.getId());
 		button.save();
+		
+		// 创建一个文件夹菜单并保存，父节点为根节点
 		Menu folder = new Menu(menuGateway);
 		folder.setName(UUID.randomUUID().toString());
 		folder.setType(MenuType.FOLDER);
 		folder.setFatherId(root.getId());
 		folder.save();
 
+		// 测试各种非法的父子关系组合
 		Menu menu = new Menu(menuGateway);
 		menu.setName(UUID.randomUUID().toString());
 		menu.setType(MenuType.BUTTON);
+		
+		// 尝试将按钮添加到不存在的节点下，应该抛出异常
 		menu.setFatherId((long) UUID.randomUUID().toString().hashCode());
 		Assertions.assertThatThrownBy(menu::save)
 			.as("文件夹下添加按钮,一定会报错")
 			.isInstanceOf(ClientException.class)
 			.hasMessage(ClientErrorCode.FATHER_IS_NOT_PAGE.getMessage());
+			
+		// 尝试将按钮添加到文件夹下，应该抛出异常
 		menu.setFatherId(folder.getId());
 		Assertions.assertThatThrownBy(menu::save)
 			.as("文件夹下添加按钮,一定会报错")
 			.isInstanceOf(ClientException.class)
 			.hasMessage(ClientErrorCode.FATHER_IS_NOT_PAGE.getMessage());
+			
+		// 尝试将页面添加到页面下，应该抛出异常
 		menu.setFatherId(page.getId());
 		menu.setType(MenuType.PAGE);
 		Assertions.assertThatThrownBy(menu::save)
 			.as("页面下添加页面,一定会报错")
 			.isInstanceOf(ClientException.class)
 			.hasMessage(ClientErrorCode.FATHER_IS_NOT_FOLDER.getMessage());
+			
+		// 尝试将文件夹添加到页面下，应该抛出异常
 		menu.setFatherId(page.getId());
 		menu.setType(MenuType.FOLDER);
 		Assertions.assertThatThrownBy(menu::save)
