715ae89b72152772c4d25315af7c314bca3b9c08
L7-BUG <ll789y@gmail.com>
2026-01-16 18:20:24 +0800

    feat(role): 添加角色实体类
    - 创建 Role 实体类用于表示系统角色信息
    - 定义角色编码、名称和全路径编码属性
    - 使用 Lombok 注解简化 getter/setter 方法
    - 添加必要的注释文档说明

---
+++ system-domain/src/main/java/com/l7bug/system/domain/role/Role.java
@@ -0,0 +1,25 @@
+package com.l7bug.system.domain.role;
+
+import lombok.Data;
+
+/**
+ * Role
+ *
+ * @author Administrator
+ * @since 2026/1/16 18:16
+ */
+@Data
+public class Role {
+	/**
+	 * 角色编码
+	 */
+	private String code;
+	/**
+	 * 角色名称
+	 */
+	private String name;
+	/**
+	 * 全路径编码
+	 */
+	private String fullCode;
+}
